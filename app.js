7a11c0792d341230c50732510459ac9f61ccf51f
Bekbolat <ergalievbek69@gmail.com>
2026-01-23 02:00:03 +0500

    done

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const blogRoutes = require('./routes/blogRoutes');
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+
+app.use('/api/blogs', blogRoutes);
+
+
+
+//middleware for error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({error: "Something is wrong"});
+} );
+
+//connecting to mongoDB
+mongoose.connect(process.env.MONGO_URI) 
+    .then(() => console.log("MongoDB is connected"))
+    .catch(err => {
+        console.error("NO CONNECTION", err.message);
+        process.exit(1);
+    });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log("Server is running!!!")
+})
---
+++ routes/blogRoutes.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const router = express.Router();
+const Blog = require('../models/Blog');
+
+// post blog(create a blog)
+router.post('/', async (req, res) => {
+    try {
+        const { title, body, author } = req.body;
+        
+        // Validation
+        if (!title || !body) {
+            return res.status(400).json({
+                error: "Title and body are required"
+            });
+        }
+        
+        const blog = new Blog({ title, body, author });
+        await blog.save();
+        
+        res.status(201).json(blog);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// get blogs(gets all the blogs)
+router.get('/', async (req, res) => {
+    try {
+        const blogs = await Blog.find().sort({createdAt: -1});
+        res.json(blogs);
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+});
+
+//get blogs by id
+router.get('/:id', async (req, res) => {
+    try {
+        const blogs = await Blog.findById(req.params.id);
+
+        if(!blog) {
+            return res.status(404).json({error: "Blog not found"});
+        }
+
+        res.json(blog)
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+});
+
+//put blogs(with id) update
+router.put('/:id', async (req, res) => {
+    try {
+        const {title, body, author} = req.body;
+
+        //validation
+        if(!title || !body) {
+            return res.status(400).json({error: "Title and body is required"});
+        }
+
+        const blog = await Blog.findByIdAndUpdate(
+            req.params.id,
+            {title, body, author},
+            {new: true, runValidators: true}
+        );
+
+        if(!blog) {
+            return res.status(404).json({error: "Blog not found"});
+        }
+
+        res.json(blog);
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+});
+
+//delete blog by id
+router.delete('/:id', async (req, res) => {
+    try {
+        const blog = await Blog.findByIdAndDelete(req.params.id);
+
+        if(!blog) {
+            return res.status(404).json({error: "Blog is not found"});
+        }
+
+        res.json({message: "Deleted"})
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+module.exports = router;
